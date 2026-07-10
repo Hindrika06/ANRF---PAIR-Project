bd93d84967c6fbcfed01fe594280ba27d123f42a
Mahesh Donga <maheshd3846@gmail.com>
2026-07-10 20:32:33 +0530

    feat: restore official ANRF-PAIR logo image (2.png) in preloader and clip-path build sequence

--- preloader.php
+++ preloader.php
@@ -1,24 +1,15 @@
 <!-- ============================================================
-     PRELOADER INLINE SVG COMPONENT
+     PRELOADER COMPONENT (OFFICIAL LOGO IMAGE)
      ============================================================ -->
 <div id="anrf-preloader" style="position: fixed; top: 0; left: 0; width: 100vw; height: 100vh; background-color: #ffffff; z-index: 999999; display: flex; align-items: center; justify-content: center; opacity: 1;">
-    <svg class="anrf-preloader-svg" viewBox="0 0 450 160" xmlns="http://www.w3.org/2000/svg">
-        <!-- 8 Concentric Semicircular Arcs centered at (80, 80) -->
-        <path class="arc-path arc-1" d="M 80 65 A 15 15 0 0 0 80 95" />
-        <path class="arc-path arc-2" d="M 80 58 A 22 22 0 0 0 80 102" />
-        <path class="arc-path arc-3" d="M 80 51 A 29 29 0 0 0 80 109" />
-        <path class="arc-path arc-4" d="M 80 44 A 36 36 0 0 0 80 116" />
-        <path class="arc-path arc-5" d="M 80 37 A 43 43 0 0 0 80 123" />
-        <path class="arc-path arc-6" d="M 80 30 A 50 50 0 0 0 80 130" />
-        <path class="arc-path arc-7" d="M 80 23 A 57 57 0 0 0 80 137" />
-        <path class="arc-path arc-8" d="M 80 16 A 64 64 0 0 0 80 144" />
-        
-        <!-- Red Central Dot -->
-        <circle class="dot-path" cx="80" cy="80" r="7.5" />
-        
-        <!-- Deep Purple / Red Text "ANRF-PAIR" -->
-        <text class="text-path" x="160" y="92" font-family="'Montserrat', sans-serif" font-size="34" font-weight="900" letter-spacing="1px">
-            <tspan fill="#6F2C91">ANRF</tspan><tspan fill="#BC2121">-PAIR</tspan>
-        </text>
-    </svg>
+    <div class="anrf-preloader-logo-container">
+        <!-- Base logo acts as sizing reference -->
+        <img src="2.png" alt="ANRF-PAIR Logo" class="anrf-preloader-base">
+        <!-- Animated overlay layers using the official logo image -->
+        <img src="2.png" alt="" class="anrf-preloader-layer preloader-arc-3">
+        <img src="2.png" alt="" class="anrf-preloader-layer preloader-arc-2">
+        <img src="2.png" alt="" class="anrf-preloader-layer preloader-arc-1">
+        <img src="2.png" alt="" class="anrf-preloader-layer preloader-dot">
+        <img src="2.png" alt="" class="anrf-preloader-layer preloader-text">
+    </div>
 </div>
