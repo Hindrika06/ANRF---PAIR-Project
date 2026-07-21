430bd55d9aaeae3daa1795b34415223cdb1104d3
Mahesh Donga <maheshd3846@gmail.com>
2026-07-21 16:13:27 +0530

    Add update_passwords.sql script for user password replacement

---
+++ update_passwords.sql
@@ -0,0 +1,8 @@
+UPDATE `users` SET `password` = '$2y$10$4kD5WIg5HeeaUiSXSuF.reP8kFjNZpS.TLPds8aGSTlMgolyGWTM2' WHERE `username` = 'admin@cuk.ac.in';
+UPDATE `users` SET `password` = '$2y$10$1iyp4cFyn.J16Zc1EvU1AObdvTPNntzWi7BWjVcBslLZWQHbnSEaS' WHERE `username` = 'Idsathyan@cuk.ac.in';
+UPDATE `users` SET `password` = '$2y$10$eHGJm/WkNUSrkbvaL3kqIu.HXKEjGZUzmr.8IyVqSohMVFDC4KKuq' WHERE `username` = 'anupkesavan@kannuriuniv.ac.in';
+UPDATE `users` SET `password` = '$2y$10$gI916sYvvv1RAn0zSodsw.EJMYo6DqNaPM12kQTQEl8W0YabMt9VC' WHERE `username` = 'radhakrishnanek@mgu.ac.in';
+UPDATE `users` SET `password` = '$2y$10$kaYS.I5rxDdmcxJD4ZGdRuyz/lsLMsSV3TlOf/nevFKf7MgqsFrzi' WHERE `username` = 'vijjulatha@osmania.ac.in';
+UPDATE `users` SET `password` = '$2y$10$tNzaoXWIvOj2Gu6D3pqHjutfDCBsG.B10tPs1436ozSJrgE2gOCW.' WHERE `username` = 'balaji.meriga@gmail.com';
+UPDATE `users` SET `password` = '$2y$10$gx/vkviq9X6U7UZAzAZI.O1TGU4aYUNdzBkT5rh0CVryykSZojVwO' WHERE `username` = 'admin@uoh.ac.in';
+UPDATE `users` SET `password` = '$2y$10$xgB44LC4ZBvY6z6u.0ioMOI3nDuMK36p8GvkyZAEqSsaN5TYeOJt6' WHERE `username` = 'sarma7@yogivemanauniversity.ac.in';
